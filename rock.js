e5f5ba06cd0a89fabd7a3c9c9ef925057f768285
Jesari82 <jries@js-ries.de>
2024-06-05 14:41:08 +0200

    Letzter Feinschliff

--- rock.js
+++ rock.js
@@ -1,12 +1,6 @@
-console.log()
-console.log('Schere-Stein-Papier')
-console.log()
+let player = process.argv.slice(2)[0]; //Zug des Spielers - Prozess hat einen Index von 3 (0, 1, 2). [0] greift auf das das eingegebene Element als String zu.
 
-//Zug des Spielers - Prozess hat einen Index von 3 (0, 1, 2). [0] greift auf das das eingegebene Element als String zu.
-let player = process.argv.slice(2)[0];
-
-// Computer Zufallszug -> Die 3 möglichen numbers werden über einen Switch in die Zugnahmen umgewandelt - Defaultwert für die Vollständigkeit.
-function getRandomInt() {
+function getRandomInt() { // Computer Zufallszug -> Die 3 möglichen numbers werden über einen Switch in die Zugnahmen umgewandelt - Defaultwert für die Vollständigkeit.
     const randomNumber = Math.floor(Math.random() * 3);
     switch (randomNumber){
         case 0:
@@ -19,13 +13,11 @@ function getRandomInt() {
             return "Damn it"
     }
   }
-
 const computer = getRandomInt();
 
-// If-Else Abfrage für das Ergebnis. Abfrage von True-False Werten. Hierbei müssen bei && beide Werte und bei || ein Wert true sein um als relevanter Wert erkannt zu werden.
-if (player === computer) {
-    console.log(`Das Spiel endet unentschieden! ${player} : ${computer}`)}
+if (player === computer) { // If-Else Abfrage für das Ergebnis. Abfrage von True-False Werten. Hierbei müssen bei && beide Werte und bei || ein Wert true sein um als relevanter Wert erkannt zu werden.
+    console.log(`Das Spiel endet unentschieden! Spieler wählte "${player}" und der Computer hat "${computer}".`)}
 else if (player === "Stein" && computer === "Schere" || player === "Schere" && computer === "Papier" || player === "Papier" && computer === "Stein") {
-    console.log(`Der Spieler gewinnt! ${player} : ${computer}`)}
+    console.log(`Der Spieler gewinnt! Spieler wählte "${player}" und der Computer hat "${computer}".`)}
 else {
-    console.log(`Der Computer gewinnt! ${computer} : ${player}`)}
+    console.log(`Der Computer gewinnt! Der Computer hat "${computer}" und Spieler wählte "${player}".`)}
